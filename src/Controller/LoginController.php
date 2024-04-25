e3b21e12dced238f0f6e8618eed31e0c0339a9bf
in-ASoriano <andreu.soriano@sklum.com>
2024-04-25 14:22:44 +0200

    Implementat el validador del dni i el paquet de security de symfony per al login

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Customer;
+use App\Form\CustomerType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils, AuthorizationCheckerInterface $authorizationChecker): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+        $customer = new Customer();
+        $formCustomer = $this->createForm(CustomerType::class, $customer);
+
+        if ($authorizationChecker->isGranted('ROLE_LOGIN')) {
+            return $this->redirectToRoute('app_default');
+        }
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'formCustomer' => $formCustomer,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout() {
+
+    }
+}
---
+++ src/Form/CustomerType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Customer;
+use App\Entity\Login;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CustomerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('name')
+            ->add('lastname')
+            ->add('phone')
+            ->add('address')
+            ->add('dni')
+            ->add('login', LoginType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Customer::class,
+        ]);
+    }
+}
