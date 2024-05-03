0828f39d1989cd1495823ca50c0b76d89513bf4b
in-ASoriano <andreu.soriano@sklum.com>
2024-05-03 16:42:30 +0200

    update

--- src/Form/VehicleType.php
+++ src/Form/VehicleType.php
@@ -8,6 +8,7 @@ use App\Entity\Provider;
 use App\Entity\Vehicle;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,13 +18,25 @@ class VehicleType extends AbstractType
     {
         $builder
             ->add('plate')
-            ->add('fuel')
+            ->add('fuel', ChoiceType::class,[
+                'choices' => [
+                    'Diesel' => 'diesel',
+                    'Gasolina' => 'gasolina',
+                    'Hybrid' => 'hybrid',
+                    'Electric' => 'electric',
+                ]
+            ])
             ->add('color')
             ->add('price_per_day')
             ->add('available')
             ->add('doors')
             ->add('capacity')
-            ->add('transmission')
+            ->add('transmission', ChoiceType::class,[
+                'choices' => [
+                    'Manual' => 'manual',
+                    'Automatic' => 'automatic',
+                ]
+            ])
             ->add('description')
             ->add('category')
             ->add('isDeleted')
