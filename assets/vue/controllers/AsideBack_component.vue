090aabcd59945dc96686b8dbd2ba873b02edb211
andreu <andreusoriano2@gmail.com>
2024-05-16 20:33:10 +0200

    Vue no funciona. Creacio del crud de reservation

--- assets/vue/controllers/AsideBack_component.vue
+++ assets/vue/controllers/AsideBack_component.vue
@@ -15,11 +15,46 @@ defineProps({
 </script>
 
 <template>
-  <nav class="bg-quaternary-lm">
-    <ul class="m-0 list-styled fs-5 d-flex flex-column text-sm-start text-center">
-      <li class="pe-3 py-4 bg-tertiary-lm ps-sm-5">
+  <nav class="bg-primary-lm">
+    <ul class="m-0 list-unstyled fs-5 d-flex flex-column text-sm-start text-center font-lm-text">
+      <li class="pe-3 py-4 ps-sm-5 btn-normal-hover">
         <a :href="Routes.home" class="text-decoration-none text-white">
-          <i class="bi bi-file-earmark"></i> Factures
+          <i class="bi bi-house"></i> Inicio
+        </a>
+      </li>
+      <li class="pe-3 py-4 ps-sm-5 btn-normal-hover">
+        <a :href="Routes.home" class="text-decoration-none text-white">
+          <i class="bi bi-file-earmark"></i> Facturas
+        </a>
+      </li>
+      <li class="pe-3 py-4 ps-sm-5 btn-normal-hover">
+        <a :href="Routes.home" class="text-decoration-none text-white">
+          <i class="bi bi-person"></i> Clientes
+        </a>
+      </li>
+      <li class="pe-3 py-4 ps-sm-5 btn-normal-hover">
+        <a :href="Routes.home" class="text-decoration-none text-white">
+          <i class="bi bi-file-earmark-person"></i> Empleados
+        </a>
+      </li>
+      <li class="pe-3 py-4 ps-sm-5 btn-normal-hover">
+        <a :href="Routes.home" class="text-decoration-none text-white">
+          <i class="bi bi-calendar"></i> Reservas
+        </a>
+      </li>
+      <li class="pe-3 py-4 ps-sm-5 btn-normal-hover">
+        <a :href="Routes.home" class="text-decoration-none text-white">
+          <i class="bi bi-car-front-fill"></i> Vehículos
+        </a>
+      </li>
+      <li class="pe-3 py-4 ps-sm-5 btn-normal-hover">
+        <a :href="Routes.home" class="text-decoration-none text-white">
+          <i class="bi bi-truck"></i> Proveedores
+        </a>
+      </li>
+      <li class="pe-3 py-4 ps-sm-5 btn-normal-hover">
+        <a :href="Routes.home" class="text-decoration-none text-white">
+          <i class="bi bi-cart"></i> Pedidos
         </a>
       </li>
     </ul>
--- src/Form/ReservationType.php
+++ src/Form/ReservationType.php
@@ -3,26 +3,37 @@
 namespace App\Form;
 
 use App\Entity\Customer;
-use App\Entity\Invoice;
+use App\Entity\PaymentDetails;
+use App\Entity\Reservation;
+use App\Entity\Vehicle;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class InvoiceType extends AbstractType
+class ReservationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('number')
-            ->add('price')
-            ->add('date', null, [
+            ->add('start_date', null, [
                 'widget' => 'single_text',
+            ])
+            ->add('end_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('total_price')
+            ->add('vehicle', EntityType::class, [
+                'class' => Vehicle::class,
+                'choice_label' => 'id',
+            ])
+            ->add('paymentDetails', EntityType::class, [
+                'class' => PaymentDetails::class,
+                'choice_label' => 'id',
             ])
             ->add('customer', EntityType::class, [
                 'class' => Customer::class,
                 'choice_label' => 'id',
-                'placeholder' => 'Seleccione un cliente'
             ])
         ;
     }
@@ -30,7 +41,7 @@ class InvoiceType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Invoice::class,
+            'data_class' => Reservation::class,
         ]);
     }
 }
