c569161653a715078d5fb3ce6e826f754a6052d7
andreuador <andreusoriano2@gmail.com>
2024-05-20 19:34:12 +0200

    Falta que ixquen les imatges dels vehicles en el cataleg. També s'ha creat el controlador de detalls del coche

---
+++ src/Controller/DetailsVehicleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Vehicle;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/details/vehicle')]
+class DetailsVehicleController extends AbstractController
+{
+    #[Route('/{id}', name: 'app_details_vehicle')]
+    public function index(Vehicle $vehicle): Response
+    {
+        return $this->render('details_vehicle/index.html.twig', [
+            'vehicle' => $vehicle,
+        ]);
+    }
+}
