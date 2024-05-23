0571f5050679e8341452da4bda5436a5d88c2571
andreuador <andreusoriano2@gmail.com>
2024-05-23 22:30:02 +0200

    Falta que en la base de dades es relacione be la ordre en customer i la reserva en Garage Controller

--- src/Controller/GarageController.php
+++ src/Controller/GarageController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Invoice;
 use App\Entity\Vehicle;
 use App\Repository\InvoiceRepository;
 use App\Repository\OrderRepository;
+use App\Repository\ReservationRepository;
 use App\Repository\VehicleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -70,7 +72,8 @@ class GarageController extends AbstractController
     }
 
     #[Route('/delete/{id}', name: 'app_garage_delete_vehicle')]
-    public function remove($id, VehicleRepository $vehicleRepository, EntityManagerInterface $entityManager): Response {
+    public function remove($id, VehicleRepository $vehicleRepository, EntityManagerInterface $entityManager): Response
+    {
         $vehicleId = $id;
         $vehicle = $vehicleRepository->find($vehicleId);
         $vehicle->setVehicleOrder(null);
@@ -79,5 +82,61 @@ class GarageController extends AbstractController
         $entityManager->flush();
 
         return $this->redirectToRoute('app_garage', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/checkout', name: 'app_garage_checkout', methods: ['POST', 'GET'])]
+    public function checkout(OrderRepository $orderRepository, ReservationRepository $reservationRepository, EntityManagerInterface $entityManager): Response
+    {
+        // Obtener el pedido pendiente del usuario
+        $login = $this->getUser();
+        $customer = $login->getCustomer();
+        $pendingOrder = $orderRepository->findOneBy(['state' => 'En proceso', 'customer' => $customer]);
+
+        if (!$pendingOrder) {
+            // Manejar el caso en que no haya un pedido pendiente
+            $this->addFlash('error', 'No hay un pedido pendiente para completar.');
+            return $this->redirectToRoute('app_garage');
+        }
+
+        // Calcular el precio total del carrito
+        $totalPrice = 0;
+        foreach ($pendingOrder->getVehicle() as $vehicle) {
+            $totalPrice += $vehicle->getPricePerDay();
+        }
+
+        // Crear una nueva factura
+        $invoice = new Invoice();
+
+        // Obtener el último número de factura y aumentarlo en uno
+        $lastInvoice = $entityManager->getRepository(Invoice::class)->findOneBy([], ['id' => 'DESC']);
+        $lastNumber = $lastInvoice ? $lastInvoice->getNumber() : 0;
+        $invoice->setNumber($lastNumber + 1);
+
+        // Establecer el precio en base al total del carrito
+        $invoice->setPrice($totalPrice);
+
+        $invoice->setDate(new \DateTime());
+        $invoice->setDeleted(false);
+
+        $invoice->setInvoiceOrder($pendingOrder);
+        $invoice->setCustomer($customer);
+        $entityManager->persist($invoice);
+        $entityManager->flush();
+
+        foreach ($pendingOrder->getReservation() as $reservation) {
+            // Establecer la fecha actual para la reserva
+            //$reservation->setDate(new \DateTime());
+            $reservation->setReservationOrder($pendingOrder);
+            // Asignar el cliente a la reserva
+            $reservation->setCustomer($customer);
+            $entityManager->persist($reservation);
+        }
+
+        // Actualizar el estado del pedido a "Completado"
+        $pendingOrder->setState('Completado');
+        $entityManager->persist($pendingOrder);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_default', ['id' => $invoice->getId()]);
     }
 }
