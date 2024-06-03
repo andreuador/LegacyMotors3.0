93a541191fef6f570577c2d9f81044340a0ae91f
andreuador <andreusoriano2@gmail.com>
2024-06-03 15:11:53 +0200

    solucionant coses

--- src/Form/EmployeeType.php
+++ src/Form/EmployeeType.php
@@ -6,16 +6,34 @@ use App\Entity\Employee;
 use App\Entity\Login;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Range;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class EmployeeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('lastname')
+            ->add('name', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('email', TextType::class)
+            ->add('phone', TextType::class)
+            ->add('salary', NumberType::class, [
+                'constraints' => [
+                    new Range([
+                        'min' => 1000,
+                        'max' => 2000,
+                    ]),
+                    new Regex([
+                        'pattern' => '/^\d+$/',
+                        'message' => 'Este valor debería ser un número.',
+                    ]),
+                ],
+            ])
             ->add('login', LoginType::class)
         ;
     }
